8219ea1e21c56d16bd9baa1c01ce16b859f74e60
Govind Sharma <govindpokhrelsharma@cedcommerce.com>
2022-01-28 18:23:26 +0530

    Final Magento Build

--- scripts/magento.sh
+++ scripts/magento.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd /var/www/html/magento-community-aws
-composer install --ignore-platform-reqs
+composer install --ignore-platform-reqs > /var/www/html/magento-community-aws/var/log/composer.log
 php  bin/magento maintenance:enable
-php  bin/magento setup:upgrade
-php  bin/magento deploy:mode:set production
+php  bin/magento setup:upgrade > /var/www/html/magento-community-aws/var/log/setupupgrade.log
+php  bin/magento deploy:mode:set production > /var/www/html/magento-community-aws/var/log/deployproduction.log
 php  bin/magento cache:clean
 php  bin/magento cache:flush
 php  bin/magento maintenance:disable
 chmod -R 777 var generated pub/static
-
